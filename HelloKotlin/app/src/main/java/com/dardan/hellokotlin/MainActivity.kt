19d17e39116a5053a434881cc1edea750da9e5d9
darwinQSot <quispesotodaniel@gmail.com>
2021-11-05 00:04:06 -0500

    Seccion 2 - PrimerApp: Hola mundo - Lectura de texto con asistente

--- HelloKotlin/app/src/main/java/com/dardan/hellokotlin/MainActivity.kt
+++ HelloKotlin/app/src/main/java/com/dardan/hellokotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import java.util.*
 
@@ -15,8 +16,8 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         setContentView(R.layout.activity_main)
 
         tts = TextToSpeech(this, this)
-        var message: String = findViewById<TextView>(R.id.textView).text.toString()
-        Log.i("message textview", message)
+
+        findViewById<Button>(R.id.btnPlay).setOnClickListener{speak()}
     }
 
     private fun speak(){
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     }
     override fun onInit(status: Int) {
         if(status == TextToSpeech.SUCCESS){
-            findViewById<TextView>(R.id.textView).text = "Listo!!"
+            findViewById<TextView>(R.id.textView).text = "Hello Kotlin!!"
             tts!!.setLanguage(Locale.US)
         } else {
             findViewById<TextView>(R.id.textView).text = "No Disponible!"
