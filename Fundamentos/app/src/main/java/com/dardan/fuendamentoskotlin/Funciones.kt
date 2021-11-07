e007526bd394bcb368a122b10e1124db7027ae05
darwinQSot <quispesotodaniel@gmail.com>
2021-11-07 00:19:57 -0500

    Seccion 3 - PrimerApp: Datos Funciones de nivel medio

---
+++ Fundamentos/app/src/main/java/com/dardan/fuendamentoskotlin/Funciones.kt
@@ -0,0 +1,11 @@
+package com.dardan.fuendamentoskotlin
+
+fun main(){
+    sayHello()
+
+    newTopic("Arguemntos")
+}
+
+private fun sayHello(): Unit {  //Unit = void y el metodo es por defecto void
+    println("Hola Kotlin")
+}
