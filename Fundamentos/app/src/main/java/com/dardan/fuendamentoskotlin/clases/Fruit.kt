f766982bcbdeed803594047e7c8372c2de9b504d
darwinQSot <quispesotodaniel@gmail.com>
2021-11-07 02:07:16 -0500

    Seccion 4 - POO en Kotlin: Fin de sección

---
+++ Fundamentos/app/src/main/java/com/dardan/fuendamentoskotlin/clases/Fruit.kt
@@ -0,0 +1,4 @@
+package com.dardan.fuendamentoskotlin.clases
+
+data class Fruit(val id: Long, var name: String, var color: String, var peso: Int) { //data class diseñana para un correcto modelo de datos
+}
