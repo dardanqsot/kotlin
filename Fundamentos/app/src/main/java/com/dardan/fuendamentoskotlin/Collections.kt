0458d0dfcb6d31d11790e5926fc1aa5afcf6cb9a
darwinQSot <quispesotodaniel@gmail.com>
2021-11-07 01:51:16 -0500

    Seccion 4 - POO en Kotlin: MutableListOf(estructuras dinámicas)

--- Fundamentos/app/src/main/java/com/dardan/fuendamentoskotlin/Collections.kt
+++ Fundamentos/app/src/main/java/com/dardan/fuendamentoskotlin/Collections.kt
@@ -1,5 +1,8 @@
 package com.dardan.fuendamentoskotlin
 
+import com.dardan.fuendamentoskotlin.clases.Group
+import com.dardan.fuendamentoskotlin.clases.User
+
 fun main() {
     newTopic("Colecciones")
     newTopic("Solo Lectura")
@@ -7,4 +10,27 @@ fun main() {
     println(frutaList.get((0..frutaList.size-1).random()))
     println("Index de Uva es ${frutaList.indexOf("Uva")}")
 
+    newTopic("Mutable List")
+    val myUser = User(0,"Darwin", "Quispe", Group.FAMILY.ordinal)
+    val bro = myUser.copy(1, "Daniel")
+    val friend = bro.copy(2, group = Group.FRIEND.ordinal)
+
+    val usersList = mutableListOf(myUser, bro)
+    println(usersList)
+    usersList.add(friend)
+    println(usersList)
+    //usersList.removeAt(1)
+    usersList.remove(bro)
+    println(usersList)
+
+    val userSelectedList = mutableListOf<User>()
+    println(userSelectedList)
+    //userSelectedList.addAll(usersList)
+    userSelectedList.add(myUser)
+    println(userSelectedList)
+    userSelectedList.set(0,bro)
+    println(userSelectedList)
+    userSelectedList.add(myUser)
+    userSelectedList.add(myUser)
+    println(userSelectedList)
 }
