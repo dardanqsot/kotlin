fdf9903ef940790a22a13a3e761d3d8771be1623
darwinQSot <quispesotodaniel@gmail.com>
2021-11-07 00:25:07 -0500

    Seccion 3 - PrimerApp: Datos Retorno de datos

--- Fundamentos/app/src/main/java/com/dardan/fuendamentoskotlin/Funciones.kt
+++ Fundamentos/app/src/main/java/com/dardan/fuendamentoskotlin/Funciones.kt
@@ -3,9 +3,20 @@ package com.dardan.fuendamentoskotlin
 fun main(){
     sayHello()
 
-    newTopic("Arguemntos")
+    newTopic("Argumentos")
+    //println(sum(2,3))
+    val a = 2
+    val b = 3
+    println("$a + $b = ${sum(a,b)}")
+    println("$a - $b = ${sub(a,b)}")
 }
 
 private fun sayHello(): Unit {  //Unit = void y el metodo es por defecto void
     println("Hola Kotlin")
 }
+
+fun sum(a: Int, b: Int): Int{
+    return a + b
+}
+
+fun sub(a: Int, b: Int) = a - b
