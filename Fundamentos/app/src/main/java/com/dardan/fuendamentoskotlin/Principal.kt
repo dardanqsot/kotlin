7b83ccd0301e7d141832251a20913ef1dc7c744e
darwinQSot <quispesotodaniel@gmail.com>
2021-11-06 22:29:01 -0500

    Seccion 3 - PrimerApp: Contantes y Variables

--- Fundamentos/app/src/main/java/com/dardan/fuendamentoskotlin/Principal.kt
+++ Fundamentos/app/src/main/java/com/dardan/fuendamentoskotlin/Principal.kt
@@ -3,6 +3,15 @@ package com.dardan.fuendamentoskotlin
 fun main(){
     //print("Hola Kotlin")
     newTopic("Hola Kotlin")
+
+    newTopic("Variables y constantes")
+    //val a = 2
+    val a = "Hola"
+    println(a)
+
+    var b: Int
+    b = 5
+    println("b = $b")
 }
 
 fun newTopic(topic: String){
