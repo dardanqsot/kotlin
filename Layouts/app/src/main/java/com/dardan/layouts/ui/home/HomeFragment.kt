ac52314c4ad6c3c1dab767f4ea96380f680f0ce8
darwinQSot <quispesotodaniel@gmail.com>
2021-11-08 23:59:33 -0500

    Sección 5: Layouts - LinearLayout

--- Layouts/app/src/main/java/com/dardan/layouts/ui/home/HomeFragment.kt
+++ Layouts/app/src/main/java/com/dardan/layouts/ui/home/HomeFragment.kt
@@ -9,12 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.dardan.layouts.R
-import com.dardan.layouts.databinding.FragmentHomeBinding
+import com.dardan.layouts.databinding.FragmentLinearBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentLinearBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +28,7 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentLinearBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
