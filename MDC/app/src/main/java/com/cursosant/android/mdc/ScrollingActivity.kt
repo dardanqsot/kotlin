2b17cc4a89b7494fc712a58e76b893585ccca353
darwinQSot <quispesotodaniel@gmail.com>
2022-01-07 23:29:20 -0500

    Sesion 6 : Material Design Components

---
+++ MDC/app/src/main/java/com/cursosant/android/mdc/ScrollingActivity.kt
@@ -0,0 +1,119 @@
+package com.cursosant.android.mdc
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.webkit.URLUtil
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.cursosant.android.mdc.databinding.ActivityScrollingBinding
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.snackbar.Snackbar
+
+class ScrollingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_scrolling)
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            if (findViewById<BottomAppBar>(R.id.bottom_app_bar).fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER){
+                findViewById<BottomAppBar>(R.id.bottom_app_bar).fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+            } else {
+                findViewById<BottomAppBar>(R.id.bottom_app_bar).fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+            }
+        }*/
+        binding.fab.setOnClickListener {
+            if (binding.bottomAppBar.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER){
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+            } else {
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+            }
+        }
+
+        binding.bottomAppBar.setNavigationOnClickListener({
+            Snackbar.make(binding.root, R.string.message_action_success, Snackbar.LENGTH_LONG)
+                    .setAnchorView(binding.fab)
+                    .show()
+        })
+
+        binding.content.btnSkip.setOnClickListener { binding.content.cvAd.visibility = View.GONE }
+
+        binding.content.btnBuy.setOnClickListener {
+            Snackbar.make(it, R.string.card_buying, Snackbar.LENGTH_LONG)
+                    .setAnchorView(binding.fab)
+                    .setAction(R.string.card_to_go, {
+                        Toast.makeText(this, R.string.card_historial, Toast.LENGTH_SHORT).show()
+                    })
+                    .show()
+        }
+
+        loadImage()
+
+        binding.content.cbEnablePass.setOnClickListener{
+            binding.content.tilPassword.isEnabled = !binding.content.tilPassword.isEnabled
+        }
+
+        binding.content.cbEnablePass.isChecked
+
+        binding.content.etUrl.onFocusChangeListener = View.OnFocusChangeListener { view, focused ->
+            var errorStr: String? = null
+            val url = binding.content.etUrl.text.toString()
+
+            if (!focused){
+                if (url.isEmpty()){
+                     errorStr = getString(R.string.card_required)
+                } else if (URLUtil.isValidUrl(url)){
+                    loadImage(url)
+                } else {
+                    errorStr = getString(R.string.card_invalid_url)
+                }
+            }
+
+            binding.content.tilUrl.error = errorStr
+        }
+
+        binding.content.toogleButton.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            binding.content.root.setBackgroundColor(
+                    when(checkedId){
+                        R.id.btnRed -> Color.RED
+                        R.id.btnBlue -> Color.BLUE
+                        else -> Color.GREEN
+                    }
+            )
+        }
+    }
+
+    private fun loadImage(url: String = "https://i.ytimg.com/vi/uNhAHzUpsXQ/hqdefault.jpg"){
+        Glide.with(this)
+                .load(url)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .into(binding.content.imgCover)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_scrolling, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
